6fecc1613ddce4e95d4788a8de4c4b9ca9e6bcd9
Sid <sid.dara18@gmail.com>
2020-11-03 17:51:19 -0500

    I created a Constraint between the bird and a new log

---
+++ chain.js
@@ -0,0 +1,20 @@
+class Chain{
+    constructor(body1, body2){
+        var options = {
+            bodyA: body1,
+            bodyB: body2,
+            stiffness: 0.04,
+            length:10
+        }
+    
+        this.chain = Constraint.create(options);
+        World.add(world,this.chain)
+    }
+
+    display(){
+        var point1 = this.chain.bodyA.position
+        var point2 = this.chain.bodyB.position
+        
+        line(point1.x, point1.y, point2.x, point2.y)
+    }
+}
